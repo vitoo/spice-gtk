fa812c88492641005a768c72502a8953bd1223b2
osy <osy@turing.llc>
2022-03-04 15:52:48 -0800

    coroutine: add support for libucontext
    libucontext is a lightweight implementation of ucontext for platforms
    that do not have a built-in implementation. This allows us to use the
    same code to support libucontext as ucontext.

--- src/continuation.c
+++ src/continuation.c
@@ -25,10 +25,20 @@
 #endif
 
 #include <errno.h>
-#include <ucontext.h>
 #include <glib.h>
 
 #include "continuation.h"
+
+#ifdef HAVE_LIBUCONTEXT
+#include <libucontext/libucontext.h>
+#define ucontext_t libucontext_ucontext_t
+#define getcontext libucontext_getcontext
+#define setcontext libucontext_setcontext
+#define swapcontext libucontext_swapcontext
+#define makecontext libucontext_makecontext
+#else
+#include <ucontext.h>
+#endif
 
 /*
  * va_args to makecontext() must be type 'int', so passing
